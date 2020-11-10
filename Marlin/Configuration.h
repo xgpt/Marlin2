cb4bf479042ff4d88669e1c182b9ad09569b3606
xgpt <paul.n.gupta@gmail.com>
2020-11-10 02:16:11 -0500

    235 > 230 :-1:  no reason

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1127,8 +1127,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 230
+#define Y_BED_SIZE 230
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
